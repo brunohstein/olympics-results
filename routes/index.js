a0fcd908e99103ec179b45415ced3247671da46e
Bruno Stein <brunohstein@gmail.com>
2016-09-03 02:05:37 -0300

    show the total number of medals

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var database = require('../database.json');
 
+database.countries = database.countries.map(function(country) {
+  country.medals.total = country.medals.gold + country.medals.silver + country.medals.bronze;
+  return country;
+});
+
 router.get('/', function(req, res, next) {
   res.render('index', {
     countries: database.countries
